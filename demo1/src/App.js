9dea2910c16bf71d0ce64d9c1168ffbb9e90f9a1
aftabaman <44297023+aftabaman@users.noreply.github.com>
2023-05-03 18:39:21 +0530

    build

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -4,15 +4,28 @@ import Navbar from './components/Navbar.js';
 import Btn1 from './components/Btn2.js';
 import Form from './components/Form.js';
 import Alerts from './components/Alerts';
-
-
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
-    <>
-    
-    <Navbar navTitle = "TextUtils" searchBtn = "Search"/>
-    <Form formHeading = "TextUtil Form" textLabel = "Enter your text here "/>
-</>
+    <Router>
+      <Navbar navTitle = "TextUtils" searchBtn = "Search"/>
+      <Routes>
+        
+        
+        <Route exact path='/' element={<Form formHeading = "TextUtil Form" textLabel = "Enter your text here "/>}>
+
+          </Route>
+        <Route exact path='/About' element={<About/>}>
+        
+        </Route>
+
+      </Routes>
+    </Router>
   );
 }
 
--- demo1/src/components/Navbar.js
+++ demo1/src/components/Navbar.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import '../style/alertStyle.css';
-
+import {Link} from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
-    <div className='nav-bar'>
-       <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-        <a className="navbar-brand ">{props.navTitle}</a>
-        <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">{props.searchBtn}</button>
-        </form>
-        </div>
-  </nav>
+ 
+
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">{props.navTitle}</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/About">About</Link>
+        </li>
+        
+      </ul>
     </div>
+  </div>
+</nav>
   )
 }
 
